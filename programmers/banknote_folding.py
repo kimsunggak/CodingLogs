6f0928e3d4ae8247fa61d3811f86ed22a8d53f83
sunggak <ksg6419@naver.com>
2025-03-04 20:40:35 +0900

    Add solution

--- programmers/banknote_folding.py
+++ programmers/banknote_folding.py
@@ -6,4 +6,13 @@
 접근방식
 - 지폐의 크기를 반으로 나누어 가며 지갑의 크기와 비교
 - 지갑의 크기보다 작아질 때까지 반복
-"""
+""" 
+def solution(wallet, bill):
+    answer = 0
+    while max(wallet) < max(bill) or min(wallet) < min(bill):
+        if bill[0] > bill[1]:
+            bill[0] = bill[0] // 2
+        else:
+            bill[1] = bill[1] // 2
+        answer += 1
+    return answer
