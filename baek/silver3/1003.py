714732d2597ab138ff44227a50e64f8fa4e25385
sunggak <ksg6419@naver.com>
2025-03-20 18:46:05 +0900

    Study!!

--- baek/silver3/1003.py
+++ baek/silver3/1003.py
@@ -9,5 +9,25 @@
 - 동적 계획법 (Dynamic Programming) : 큰 문제를 작은 문제로 나누고, 작은 문제들의 답을 미리 계산하여
 저장해둔 뒤 필요할 때마다 재활용하여 풀이하는 방법
 - 중복 계산을 피하여 효율적으로 계산
--
 """
+import sys
+def main():
+    T = int(sys.stdin.readline().strip())
+    memory = {}
+    def f(n):
+        if n == 0:
+            return (1, 0)
+        elif n == 1:
+            return (0, 1)
+        if n in memory:
+            return memory[n]  
+        zero_n1, one_n1 = f(n-1)
+        zero_n2, one_n2 = f(n-2)
+        memory[n] = (zero_n1 + zero_n2, one_n1 + one_n2)
+        return memory[n]
+    result = []
+    for _ in range(T):
+        n = int(sys.stdin.readline().strip())
+        print(" ".join(map(str, f(n))))
+if __name__ == "__main__":
+    main()
