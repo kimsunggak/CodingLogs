cd3290308189f00241771f1d21fa0e22003f6c0f
Sunggak Kim <ksg6419@naver.com>
2025-03-21 15:49:13 +0900

    Add memoization to Fibonacci count and implement solution for minimum operations to reduce N to 1

--- baek/silver3/1003.py
+++ baek/silver3/1003.py
@@ -13,14 +13,20 @@
 import sys
 def main():
     T = int(sys.stdin.readline().strip())
+    # 계산해놓은 값들을 저장하기 위한 공간
     memory = {}
     def f(n):
+        # n 이 0이면 0의 개수 1 , 1의 개수 0
         if n == 0:
             return (1, 0)
+        # n 이 0이면 0의 개수 0 , 1의 개수 1
         elif n == 1:
             return (0, 1)
+        # 주어진 숫자가 이미 계산된 값이면 그 값을 반환 (중복된 계산을 방지하기 위함)
         if n in memory:
             return memory[n]  
+        # 피보나치 수열의 점화식을 이용하여 계산
+        # n-1 의 0의 개수와 1의 개수, n-2 의 0의 개수와 1의 개수를 더함
         zero_n1, one_n1 = f(n-1)
         zero_n2, one_n2 = f(n-2)
         memory[n] = (zero_n1 + zero_n2, one_n1 + one_n2)
